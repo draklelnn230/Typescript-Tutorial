0178529c288de6f1ef78249c9c7f1fd2cd9dc95a
Lan <h.baolan20020@gmail.com>
2022-02-24 14:20:21 +0700

    Working with Advanced Types

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,152 @@
-interface AddFn {
-    (a: number, b: number): number
+type Admin = {
+    name: string;
+    privileges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: Date;
 }
 
-let add: AddFn;
+type ElevatedEmployee = Admin & Employee;
 
-add = (n1: number, n2: number) => {
-    return n1 + n2;
-}
-interface Named {
-    readonly name?: string;
-    outputName?: string;
+const e1: ElevatedEmployee = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date()
 }
 
-interface Greet extends Named {
-    greet(phrase: string): void;
-}
+type Combinable = string | number;
+type Numeric = number | boolean;
 
-class Person implements Greet {
-    name?: string;
-    age = 30;
+type Universal = Combinable & Numeric;
 
-    constructor(name?: string) {
-        if (name) {
-            this.name = name;
-        }
+// Function Overloads
+
+function add(a: number, b: number): number
+function add(a: string, b: string): string
+function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
     }
-
-    greet(phrase: string) {
-        if (this.name) {
-            console.log(phrase + ' ' + this.name)
-        }
-        else { 
-           console.log('Hi!')
-        }
-    }
+    return a + b;
 }
 
-let user1: Greet
-user1 = new Person()
+const result =  add('Max', 'John');
 
-user1.greet('Hello')
+// --------------------------------------------- //
 
-console.log(user1)
+// Optional Chaining
+const fetchedUserData = {
+    id: 'u1',
+    name: 'Max',
+    job: {
+        title: 'CEO',
+        description: 'My own company'
+    }
+};
+
+// ------------------------- //
+console.log(fetchedUserData?.job?.title);
+
+
+// Nullish Coalescing
+const userInput =  0;
+
+const storedData = userInput ?? 'DEFAULT'
+
+console.log(userInput)
+
+// ----------------------------------------
+
+
+
+// type UnknownEmployee = Employee | Admin;
+
+// function printEmployeeInformation(emp: UnknownEmployee) {
+//     console.log('Name: ' + emp.name);
+//     // Type Guards
+//     if ('privileges' in emp) {
+//         console.log('Privileges: ' + emp.privileges);
+//     }
+//     if ('startDate' in emp) {
+//         console.log('Privileges: ' + emp.startDate);
+//     }
+// }
+
+// printEmployeeInformation(e1)
+
+// class Car {
+//     drive() {
+//         console.log('Driving....')
+//     }
+// }
+
+// class Truck {
+//     drive() {
+//         console.log('Driving a truck...')
+//     }
+
+//     loadCargo(amount: number) {
+//         console.log('Loading cargo...' + amount); 
+//     }
+// }
+
+// type Vehicle = Car | Truck;
+
+// const v1 = new Car();
+// const v2 = new Truck();
+
+// function useVehicle(vehicle: Vehicle) {
+//     vehicle.drive();
+//     if (vehicle instanceof Truck) {
+//         vehicle.loadCargo(1000);
+//     }
+// }
+
+// useVehicle(v1);
+// useVehicle(v2);
+
+// // Discriminated Unions
+// interface Bird {
+//     type: 'bird';
+//     flyingSpeed: number;
+// }
+
+// interface Horse {
+//     type: 'horse';
+//     runningSpeed: number;
+// }
+
+// type Animal = Bird | Horse;
+
+// function moveAnimal(animal: Animal) {
+//     let speed;
+//     switch (animal.type) {
+//         case 'bird':
+//             speed = animal.flyingSpeed
+//             break;
+//         case 'horse':
+//             speed = animal.runningSpeed
+//     }
+//     console.log('Moving at speed: ' + speed);
+// }
+
+// moveAnimal({type: 'bird', flyingSpeed: 10})
+
+
+// // Type Casting
+// const userInputElement = document.getElementById('user-input')! as HTMLInputElement; 
+
+// userInputElement.value = 'Hi, there'
+
+
+// // Index Properties
+// interface ErrorContainer {
+//     [prop: string]: string;
+// }
+
+// const errorBag: ErrorContainer = {
+//     email: 'Not a valid email!',
+//     username: 'Must start with a capital character!'
+// } 
