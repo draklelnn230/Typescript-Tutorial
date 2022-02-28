e8fa944050c3429c1048316cff0ecab5feba69f8
Lan <h.baolan20020@gmail.com>
2022-02-28 14:53:21 +0700

    Working with Decorators

--- src/app.ts
+++ src/app.ts
@@ -1,76 +1,203 @@
-
-
-
-// Generic Function & Constraint
-function merge<T extends object, U extends object>(objA: T, objB: U){
-    return Object.assign(objA, objB)
-}
-
-const mergeObj = merge({name: 'Max', hobbies: ['Sports']}, { age: 30 })
-
-interface Lengthy {
-    length: number;
-}
-
-
-// Another Generic Function
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-    let descriptionText = 'Got no value.';
-
-    if (element.length === 1) {
-        descriptionText = 'Got 1 Element'
-    } else if (element.length > 1) {
-        descriptionText = 'Got ' + element.length + ' elements.';
+function Logger(logString: string) {
+    console.log('LOGGER FACTORY')
+    return function(constructor: Function) {
+        console.log(logString)
+        console.log(constructor)
     }
-    return [element, descriptionText];
 }
 
-console.log(countAndDescribe(['Sports', 'Cooking']))
+function WithTemplate(template: string, hookId: string) {
+    console.log('TEMPLATE FACTORY')
+    return function<T extends {new(...args: any[]): {name: string} }>(originalConstructor: T) {
+        return class extends originalConstructor {
+            constructor(..._: any[]) {
+                super();
+                console.log('Rendering template');
+                const hookEl = document.getElementById(hookId);
+               
+                if(hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = this.name;
+                }
+            }
+        }
+    }
+}
+@Logger('LOGGING')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
+class Person {
+    name = 'Max';
 
-
-// The keyof Constraint
-
-function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
-    return obj[key]
+    constructor() {
+        console.log('Creating person object...')
+    }
 }
 
-console.log(extractAndConvert({name: 'Max'}, 'name'));
+const person = new Person();
 
+console.log(person)
 
-// Generic Classes
+// --- 
 
-class DataStorage<T> {
-    private data: T[] = [];
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator!')
+    console.log(target, propertyName)
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator!')
+    console.log(name)
+    console.log(target);
+    console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator!')
+    console.log(name)
+    console.log(target);
+    console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator!')
+    console.log(name)
+    console.log(target);
+    console.log(position);
+}
+class Product { 
+    @Log
+    title: string;
+    private _price: number;
+
+    @Log2
+    set price(val: number) {
+       if (val > 0) {
+        this._price = val;
+       } else {
+           throw new Error('Invalid price - should be positive!')
+       }
+    }
+
     
-    addItem(item: T) {
-        this.data.push(item);
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
     }
-
-    removeItem(item: T) {
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-
-    getItems() {
-        return [...this.data];
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
+        return this._price * (1 + tax);
     }
 }
 
-const textStorage = new DataStorage<string>();
+const p1 = new Product('Book', 19);
+const p2 = new Product('Book 2', 18);
 
-textStorage.addItem('Max')
-textStorage.addItem('John')
-textStorage.addItem('Levi')
-console.log(textStorage.getItems())
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+}
+class Printer {
+    message = 'This works!';
 
-const numberStorage = new DataStorage<number>();
+    @AutoBind
+    showMessage() {
+        console.log(this.message)
+    }
+}
 
-const objStorage = new DataStorage<object>();
-objStorage.addItem({name: 'Max'});
-objStorage.addItem({name: 'John'});
+const p = new Printer();
 
-console.log(objStorage)
-//....
-objStorage.removeItem({name: 'Max'})
-console.log(objStorage.getItems())
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage); 
 
+interface ValidatorConfig {
+    [property: string]: {
+        [validateProp: string]: string[]
+    }
+}
+
+const registerValidators: ValidatorConfig = {
+
+}
+
+
+
+function Required(target: any, propName: string) {
+    registerValidators[target.constructor.name] = {
+        ...registerValidators[target.constructor.name],
+        [propName]: ['required']
+    };
+}
+
+function PositiveNumber(target: any, propName: string) {
+    registerValidators[target.constructor.name] = {
+        ...registerValidators[target.constructor.name],
+        [propName]: ['positive']
+    };
+}
+
+function validate(obj: any) {
+    const objValidatorConfig = registerValidators[obj.constructor.name];
+
+    if ( !objValidatorConfig ) {
+        return true;
+    }
+    let isValid = true;
+    for (const prop in objValidatorConfig ) {
+        console.log(prop)
+        for (const validator of objValidatorConfig[prop]) {
+            switch (validator) {
+                case 'required':
+                    isValid = isValid &&!!obj[prop];
+                    break;
+                case 'positive':
+                    isValid = isValid && obj[prop] > 0;
+                    break;
+            }
+        }
+    }
+    return isValid;
+}
+
+class Course {
+
+    @Required
+    title: string;
+
+    @PositiveNumber
+    price: number;
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this.price = p;
+    }
+
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const titleEl = document.getElementById('title') as HTMLInputElement;
+    const priceEl = document.getElementById('price') as HTMLInputElement;
+
+    const title = titleEl.value;
+    const price = +priceEl.value;
+
+
+    const createdCourse = new Course(title, price);
+
+    if( !validate(createdCourse) ) {
+        alert('Invalid input, please try again.');
+        return;
+    }
+    console.log(createdCourse);
+})
 
